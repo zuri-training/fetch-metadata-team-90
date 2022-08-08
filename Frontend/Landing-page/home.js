8f36f7ddadb865eda58b08e4088d2b24a343d772
durobolu <durobolu@gmail.com>
2022-08-08 15:36:08 +0100

    Landing page header made responsive

--- Frontend/Landing-page/home.js
+++ Frontend/Landing-page/home.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav__mobile-list");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("avtive");
-  navMenu.classList.toggle("avtive");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
