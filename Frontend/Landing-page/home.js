ec85bbcfd5bb944bebefb4693ab36960ee5944d2
durobolu <durobolu@gmail.com>
2022-08-08 20:20:53 +0100

    Strated work on the accordion section of the landing page.

--- Frontend/Landing-page/home.js
+++ Frontend/Landing-page/home.js
@@ -1,3 +1,5 @@
+//This part is for the responsiveness of the navigation on mobile view and allows the hambuger to function well.
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -12,3 +14,20 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+//js code for the accordion
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeaders) => {
+  accordionItemHeaders.addEventListener("click", (event) => {
+    accordionItemHeaders.classList.toggle("active");
+    const accordionItemBody = accordionItemHeaders.nextElementSibling;
+    if (accordionItemHeaders.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
