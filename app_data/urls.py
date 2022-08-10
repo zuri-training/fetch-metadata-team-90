6e6d5c38af6bb2f47f93166e3a175674bc2897f7
Musa Dauda <marshalmusa143@gmail.com>
2022-08-10 08:01:52 +0100

    Done with Terms and Conditions

--- app_data/urls.py
+++ app_data/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
    	path('', views.HomePageView.as_view(), name='index'),
     path('contact/', views.Contact, name='contact'),
     path('contact/success', views.ContactSuccess.as_view(), name='contactsuccess'),
-    path('privacy-policies/', views.PrivacyView.as_view(), name='privacy'),
-    path('teams_conditions/', views.TeamsConditionView.as_view(), name='teams_conditions'),
+    path('privacy-policies/', views.PrivacyView.as_view(), name='privacy-policies'),
+    path('terms-conditions/', views.TermsConditionView.as_view(), name='terms-conditions'),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
     # path('dashboard/', views.CreateFilePuloadView.as_view(template_name='dashboard.html'), name='file_upload'),
     path('share/<int:pk>/', views.ShareFileUploadDetailView.as_view(), name='share'),
-    path('details/<int:pk>/', views.FileUploadDetailView.as_view(), name='details')
+    path('details/<int:pk>/', views.FileUploadDetailView.as_view(), name='details'),
 
 ]
