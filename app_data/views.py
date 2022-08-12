fe808867bc36ae8d3b45d3eccde5c65b7f7f3bee
KcPele <fidekg123@gmail.com>
2022-08-12 14:58:45 +0100

    notification

--- app_data/views.py
+++ app_data/views.py
@@ -48,13 +48,16 @@ class ContactView(View):
         if form.is_valid():
             name_email = request.POST['name']
             email = request.POST['sender_email']
+            
             subject = request.POST['subject']
             message_email = request.POST['message']
             message_email2 = f'{message_email} from {email}'
+            print(message_email2, settings.EMAIL_HOST_USER)
             try:
                 send_mail(subject, message_email2, email, 
                 [settings.EMAIL_HOST_USER], fail_silently=False,)
                 messages.success(request, 'Thanks, A response has been sent to your mail')
+                
                 return render(request, self.template_name, self.context)
             except:
                 messages.error(request, 'Email not sent, Something went wrong. Try again!')
